b091d1d811541221ef3f32fd95f78c473da1f9c2
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2018-10-05 08:39:43 +0000

    Split BlueExposureStatic project out of BlueExposure, and add project to carbon solution.
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/BlueExposure/": change = 1397999]

--- RegenerateProjectFile.cmd
+++ RegenerateProjectFile.cmd
@@ -2,12 +2,15 @@
 echo Checking out project and filters file
 p4 edit BlueExposure.vcxproj
 p4 edit BlueExposure.vcxproj.filters
+p4 edit BlueExposureStatic.vcxproj
+p4 edit BlueExposureStatic.vcxproj.filters
 p4 edit BlueExposure.v110_xp.vcxproj
 p4 edit BlueExposure.v110_xp.vcxproj.filters
 p4 edit BlueExposure.orbis.vcxproj
 p4 edit BlueExposure.orbis.vcxproj.filters
 echo Regenerating
 ..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i BlueExposure.ccpproj --toolset=v100
+..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i BlueExposureStatic.ccpproj --toolset=v100
 ..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i BlueExposure.ccpproj --toolset=v110_xp
 ..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i BlueExposure.ccpproj --orbis
 pause
