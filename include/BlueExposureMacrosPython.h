61ca5e768c14413b0f8d0185f879a66770b8934f
Hrafn Jökull Geirsson <5185245+hrafn@users.noreply.github.com>
2023-11-13 16:12:21 +0000

    Add __dir__() method for BlueWrapped objects
    https://ccpgames.atlassian.net/browse/PLAT-3840 (dir() function broken for BlueExposure classes)
    
    - Add a custom __dir__() method for BlueWrapped objects.
    - Register methods and members in tp_methods and tp_members respectively. This is so that members and methods show up on the object types __dict__, which is relevant because keys on the __dict__ show up in the results dir() is called on a type.
    - Add tests for dir() functionality, so that we know what behavior we expect and can tell next time when it breaks.

--- include/BlueExposureMacrosPython.h
+++ include/BlueExposureMacrosPython.h
@@ -33,6 +33,8 @@
 	static std::vector<Be::VarEntry> s_attributes; \
 	static std::vector<Be::InterfaceEntry> s_interfaces; \
 	static std::map<std::string, Be::BlueExposureFunctionSignature> s_signatures; \
+	static std::vector<PyMemberDef> s_memberDefs; \
+	static std::vector<PyMethodDef> s_methodDefs; \
 	Be::InterfaceEntry rootEntry = {&s_iroot, BLUE_INTERFACEOFFSET(_Class)}; \
 	s_interfaces.push_back( rootEntry ); \
 	Be::InterfaceEntry myEntry = {&s_iid, BLUE_INTERFACEOFFSET(_Class)}; \
@@ -47,6 +49,8 @@
 	s_interfaces.push_back( interfacesEnd ); \
 	\
 	BlueInitializePyType( &s_pyType, &_tmpclsid, _tmpdoc, &s_interfaces[0], PyNew ); \
+	BlueRegisterPyMethodDefs( &s_pyType, &s_methods[0], &s_interfaces[0], &s_methodDefs ); \
+	BlueRegisterPyMemberDefs( &s_pyType, &s_attributes[0], &s_memberDefs ); \
 	\
 	static Be::ClassInfo _classinfo; \
 	_classinfo.mClassId = &_tmpclsid;  \
