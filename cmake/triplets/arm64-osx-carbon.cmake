57aa94b8a34cb6ad5305f3989ac6e716234d0bf3
ccp-serpent <162123512+ccp-serpent@users.noreply.github.com>
2025-03-31 14:08:49 +0000

    Add `vcpkg` triplet files

---
+++ cmake/triplets/arm64-osx-carbon.cmake
@@ -0,0 +1,16 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
+set(VCPKG_OSX_ARCHITECTURES "arm64")
+
+set(VCPKG_ENV_PASSTHROUGH CCP_EVE_PERFORCE_BRANCH_PATH)
+
+if (PORT MATCHES "python3")
+    set(VCPKG_BUILD_TYPE release)
+endif()
+
+if (PORT MATCHES "greenlet")
+    set(VCPKG_BUILD_TYPE release)
+endif()
