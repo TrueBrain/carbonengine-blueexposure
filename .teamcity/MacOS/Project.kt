0587da12833e3802cd3cb66d7102237a3410803c
ccp-serpent <162123512+ccp-serpent@users.noreply.github.com>
2025-08-14 10:53:27 +0000

    Set `INSTALL_TO_MONOLITH` to off by default
    This allows the component to install through vcpkg in the correct manner
    while allowing the corresponding TC project to publish an installation
    into the monolith.

--- .teamcity/MacOS/Project.kt
+++ .teamcity/MacOS/Project.kt
@@ -84,7 +84,7 @@ class CarbonBuildMacOS(buildName: String, configType: String, preset: String) : 
         exec {
             name = "Configure"
             path = "cmake"
-            arguments = "--preset %env.CMAKE_PRESET% -S %teamcity.build.checkoutDir%/%github_checkout_folder% -B %env.CMAKE_BUILD_FOLDER% -D CMAKE_INSTALL_PREFIX=%env.CMAKE_INSTALL_PREFIX%"
+            arguments = "--preset %env.CMAKE_PRESET% -S %teamcity.build.checkoutDir%/%github_checkout_folder% -B %env.CMAKE_BUILD_FOLDER% -D CMAKE_INSTALL_PREFIX=%env.CMAKE_INSTALL_PREFIX% -DINSTALL_TO_MONOLITH=ON"
         }
         exec {
             name = "Build"
